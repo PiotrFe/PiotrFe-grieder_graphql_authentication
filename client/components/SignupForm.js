0618592e5294688fe952c0c4023868c60c32d4fd
PiotrFe <38947052+PiotrFe@users.noreply.github.com>
2020-11-08 15:20:33 +0100

    Lesson

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import AuthForm from "./AuthForm";
-import mutation from "../mutations/Login";
 import { graphql } from "react-apollo";
+import mutation from "../mutations/Signup";
 import query from "../queries/CurrentUser";
 import { hashHistory } from "react-router";
 
-class LoginForm extends Component {
+class SignupForm extends Component {
   constructor(props) {
     super(props);
 
@@ -13,8 +13,7 @@ class LoginForm extends Component {
   }
 
   componentWillUpdate(nextProps) {
-    console.log(this.props, nextProps);
-    if (!this.props.data.user && nextProps.data.user) {
+    if (nextProps.data.user && !this.props.user) {
       hashHistory.push("/dashboard");
     }
   }
@@ -26,7 +25,7 @@ class LoginForm extends Component {
         refetchQueries: [{ query }],
       })
       .catch((res) => {
-        const errors = res.graphQLErrors.map((err) => err.message);
+        const errors = res.graphQLErrors.map((error) => error.message);
         this.setState({ errors });
       });
   }
@@ -34,7 +33,7 @@ class LoginForm extends Component {
   render() {
     return (
       <div>
-        <h3>Login</h3>
+        <h3>Sign Up</h3>
         <AuthForm
           errors={this.state.errors}
           onSubmit={this.onSubmit.bind(this)}
@@ -44,4 +43,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(query)(graphql(mutation)(LoginForm));
+export default graphql(query)(graphql(mutation)(SignupForm));
